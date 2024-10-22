6cb5d92047e9b67fae00536910b7fe9aa62e18fe
Matthew Kavanagh <matthewfkavanagh@icloud.com>
2024-10-22 15:25:17 +0000

    Move global types and functions to settings files

---
+++ settings.cpp
@@ -0,0 +1,16 @@
+#include "settings.h"
+#include <cassert>
+
+RGB HexToRGB(const std::string& hex) {
+    //Create temporary color objects
+    std::string hexColor = hex;
+    assert(hexColor.length() == 6 && "Provided colorKey not a color valid hex code.\n");
+    RGB rgbColor;
+
+    //Extract sets of two characters and convert to RGB values
+    rgbColor.r = static_cast<uint8_t>(std::stoi(hexColor.substr(0,2), nullptr, 16));
+    rgbColor.g = static_cast<uint8_t>(std::stoi(hexColor.substr(2,2), nullptr, 16));
+    rgbColor.b = static_cast<uint8_t>(std::stoi(hexColor.substr(4,2), nullptr, 16));
+
+    return rgbColor;
+}
--- settings.h
+++ settings.h
@@ -1,9 +1,30 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
+#include <cstdint>
 #include <string_view>
+#include <string>
+
+//Enum type to refer to movement directions by name
+enum Direction {
+  up,       // == 0
+  right,    // == 1
+  down,     // == 2
+  left,     // == 3
+  still     // == 4
+};
+
+//Enum type to hold R,G,B color values as one object (default white)
+struct RGB {
+    uint8_t r{255};
+    uint8_t g{255};
+    uint8_t b{255};
+};
+
+RGB HexToRGB(const std::string& hex);                   //Convert hex color codes to RGB color object
 
 namespace Settings {
+    
     constexpr int SCREEN_WIDTH = 800;   //Game screen width
     constexpr int SCREEN_HEIGHT = 600;  //Game screen height
 
@@ -12,6 +33,7 @@ namespace Settings {
 
     //Background Music
     constexpr std::string_view BG_MUSIC{"relative/path/to/music.wav"};
+
 };
 
 #endif
